a8806a9af452dad79db73aec8b199026ab8510c9
Alan Baines <91232270+abaines-nextup@users.noreply.github.com>
2023-07-11 21:04:30 -0400

    add show (view) to display a single article add control logic for show add link to single article add show html add route for show

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "articles#index"
+
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
