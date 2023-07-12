e8b9815a19a736c4063ac1f7050e99ed23143231
Alan Baines <91232270+abaines-nextup@users.noreply.github.com>
2023-07-12 14:48:34 -0400

    update article model to reference comments update routes to nest comments under articles

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
